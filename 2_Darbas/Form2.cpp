881524ca7921ae1447b104cbbb95f0f1b6838e54
Dominik Zabelskij <zabelski15@gmail.com>
2025-12-25 18:40:29 +0200

    Add files via upload

---
+++ 2_Darbas/Form2.cpp
@@ -0,0 +1,63 @@
+﻿#include "pch.h"
+#include "Form2.h"
+
+#include "Studentas.h"
+#include <msclr/marshal_cppstd.h>
+#include <sstream>
+
+using namespace CppCLRWinFormsProject;
+using namespace msclr::interop;
+
+System::Void Form2::buttonSubmit_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    std::string name = marshal_as<std::string>(textBoxName->Text);
+    std::string surname = marshal_as<std::string>(textBoxSurname->Text);
+    std::string marksStr = marshal_as<std::string>(textBoxMark->Text);
+    std::string egzStr = marshal_as<std::string>(textBoxEgzam->Text);
+
+    std::vector<int> nd;
+    std::istringstream iss(marksStr);
+    int markVal;
+    while (iss >> markVal) {
+        nd.push_back(markVal);
+        if (iss.peek() == ',' ) iss.ignore();
+    }
+
+    if (name.empty() || surname.empty()) {
+        MessageBox::Show("Vardas ir pavarde negali buti tusti.", "Klaida", MessageBoxButtons::OK, MessageBoxIcon::Error);
+        return;
+    }
+    if (nd.empty()) {
+        MessageBox::Show("Iveskite bent viena namu darbu pazymi (atidarykite per tarpus arba kablelius).", "Klaida", MessageBoxButtons::OK, MessageBoxIcon::Error);
+        return;
+    }
+
+    int egz = 0;
+    try {
+        egz = std::stoi(egzStr);
+    }
+    catch (...) {
+        MessageBox::Show("Netinkamas egzamino pazymys.", "Klaida", MessageBoxButtons::OK, MessageBoxIcon::Error);
+        return;
+    }
+
+    Studentas s(name, surname, nd, egz);
+    std::ostringstream out;
+	s.skaiciuotiGalutinis();
+    if(radioButtonVidurkis->Checked){  
+        out << left << setw(15) << s.getVardas() << setw(15) << s.getPavarde() << setw(18) << std::fixed << std::setprecision(2) << s.getGalutinisVidurkis();
+    }
+    else if(radioButtonMediana->Checked){
+        out << left << setw(15) << s.getVardas() << setw(15) << s.getPavarde() << setw(18) << std::fixed << std::setprecision(2) << s.getGalutinisMediana();
+	}
+
+    this->CombinedText = gcnew System::String(out.str().c_str());
+
+    this->DialogResult = System::Windows::Forms::DialogResult::OK;
+    this->Close();
+}
+
+System::Void Form2::buttonExit_Click(System::Object^ sender, System::EventArgs^ e)
+{
+    this->Close();
+}
---
+++ 2_Darbas/Studentas.h
@@ -0,0 +1,62 @@
+﻿#ifndef STUDENTAS_H_INCLUDED
+#define STUDENTAS_H_INCLUDED
+#include "Mylib.h"
+#include "Timer.h"
+#include "Memory.h"
+
+class Studentas {
+    string vardas;
+    string pavarde;
+    vector<int> nd;
+    int egz{};
+    double galutinisVidurkis{};
+    double galutinisMediana{};
+
+public:
+    // Konstruktoriai
+    Studentas();
+    Studentas(string A, string B, vector<int> C, int D);
+
+    // Geteriai
+    string getVardas() const { return vardas; } // Taikomas vardu rušiavimui
+    string getPavarde() const { return pavarde; } // Taikomas vardu rušiavimui
+    double getGalutinisVidurkis() const { return galutinisVidurkis; } // Taikomas rušiavimui į dvi grupes
+    double getGalutinisMediana() const { return galutinisMediana; }
+
+    // Kopijavimo konstruktorius
+    Studentas(const Studentas& other);
+
+    // Priskyrimo operatorius
+    Studentas& operator=(const Studentas& other);
+
+    // Destruktorius
+    ~Studentas();
+
+    // Atsitiktinis pažymių generavimas
+    string generuotiDuomenis(size_t kiekStud = 5, int kiekNd = 5);
+
+    // Duomenų įvedimas ranka
+    void ivedimas();
+
+    // Išvedimas
+    void isvedimas() const;
+
+    // Galutinio balo skaičiavimas
+    void skaiciuotiGalutinis();
+
+    // Duomenų nuskaitymas iš failo
+    static vector<Studentas> nuskaitytiIsFailo(const string& failoVardas);
+};
+
+// Duomenų generavimas į failą
+class Generatorius {
+public:
+    static void generuotiFaila(const string& failoVardas, size_t kiekStud, int kiekNd = 5);
+    static void generuotiVisus();
+};
+
+// Surušiuotu studentų sąrašo išvedimas į failus
+void isvestiIFaila(const vector<Studentas>& sarasas, const string& failoVardas);
+
+
+#endif // STUDENTAS_H_INCLUDED
